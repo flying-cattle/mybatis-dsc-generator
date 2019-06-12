ee4eb793c9069f0a1bea821ec89eae1d08b56246
flying-cattle <695429818@qq.com>
2019-06-12 16:37:09 +0800

    提示修改

--- src/test/java/com/flying/cattle/mdg/MyGenerator.java
+++ src/test/java/com/flying/cattle/mdg/MyGenerator.java
@@ -20,28 +20,28 @@ import com.github.flying.cattle.mdg.util.MySqlToJavaUtil;
  * <p>源码地址：https://gitee.com/flying-cattle/mybatis-dsc-generator</P>
  */
 public class MyGenerator {
-	// 基础信息
-	public static final String PROJECT = "springboot-mybatis";
-	public static final String AUTHOR = "flying-cattle";
-	public static final String VERSION = "V1.0";
-	// 数据库连接信息
-	public static final String URL = "jdbc:mysql://192.168.9.246:3306/buybit_wallet?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false&useSSL=true&serverTimezone=UTC";
-	public static final String NAME = "buybit_wallet";
-	public static final String PASS = "buybit_wallet3M!";
-	public static final String DATABASE = "buybit_wallet";
-	// 类信息
-	public static final String TABLE = "user";
-	public static final String CLASSNAME = "User";
-	public static final String CLASSCOMMENT = "用户";
-	public static final String TIME = "2019年5月20日";
-	public static final String AGILE = new Date().getTime() + "";
-	// 路径信息，分开路径方便聚合工程项目，微服务项目
-	public static final String ENTITY_URL = "com.flying.cattle.entity";
-	public static final String DAO_URL = "com.flying.cattle.dao";
-	public static final String XML_URL = "com.flying.cattle.dao.impl";
-	public static final String SERVICE_URL = "com.flying.cattle.service";
-	public static final String SERVICE_IMPL_URL = "com.flying.cattle.service.impl";
-	public static final String CONTROLLER_URL = "com.flying.cattle.web";
+	// 基础信息：项目名、作者、版本
+		public static final String PROJECT = "wallet-sign"; 
+		public static final String AUTHOR = "BianPeng";
+		public static final String VERSION = "V1.0";
+		// 数据库连接信息：连接URL、用户名、秘密、数据库名
+		public static final String URL = "jdbc:mysql://127.0.0.1:3306/earn_knife?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false&useSSL=true&serverTimezone=UTC";
+		public static final String NAME = "root";
+		public static final String PASS = "pwd2020";
+		public static final String DATABASE = "earn_knife";
+		// 类信息：类名、数据库表名、类说明、时间
+		public static final String CLASSNAME = "CollectionRoute";
+		public static final String TABLE = "collection_route";
+		public static final String CLASSCOMMENT = "资金归集";
+		public static final String TIME = "2019年6月12日";
+		public static final String AGILE = new Date().getTime() + "";
+		// 路径信息，分开路径方便聚合工程项目，微服务项目
+		public static final String ENTITY_URL = "com.buybit.ws.entity";
+		public static final String DAO_URL = "com.buybit.ws.dao";
+		public static final String XML_URL = "com.buybit.ws.dao.impl";
+		public static final String SERVICE_URL = "com.buybit.ws.service";
+		public static final String SERVICE_IMPL_URL = "com.buybit.ws.service.impl";
+		public static final String CONTROLLER_URL = "com.buybit.ws.web";
 
 	public static void main(String[] args) {
 		BasisInfo bi = new BasisInfo(PROJECT, AUTHOR, VERSION, URL, NAME, PASS, DATABASE, TIME, AGILE, ENTITY_URL,
